f87ffdec17e2af5150ef0f3b3d24bbf6b5a3a7f6
Dennis <hansschwanzsxak@gmail.com>
2024-03-10 12:42:57 +0100

    side-description is changed by Cortly

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const app = new PIXI.Application();
 const ufoList = [];
 document.body.appendChild(app.view);
 
-const rocket = PIXI.Sprite.from("bilder/vodka.png");
+const rocket = PIXI.Sprite.from("bilder/car.png");
 rocket.x = 350;
-rocket.y = 465;
+rocket.y = 435;
 rocket.scale.x = 0.1;
 rocket.scale.y = 0.1;
 app.stage.addChild(rocket);
 
 gameInterval(function () {
-  const ufo = PIXI.Sprite.from("bilder/ukraine" + random(1, 2) + ".png");
+  const ufo = PIXI.Sprite.from("bilder/hinderniss" + random(1, 2) + ".png");
   ufo.x = random(0, 700);
   ufo.y = -25;
-  ufo.scale.x = 0.1;
-  ufo.scale.y = 0.1;
+  ufo.scale.x = 0.08;
+  ufo.scale.y = 0.08;
   app.stage.addChild(ufo);
   flyDown(ufo, 1);
   ufoList.push(ufo);
@@ -34,10 +34,10 @@ function rightKeyPressed() {
 
 function spaceKeyPressed() {
   const bullet = PIXI.Sprite.from("bilder/bullet.png");
-  bullet.x = rocket.x + 0;
-  bullet.y = 350;
-  bullet.scale.x = 0.2;
-  bullet.scale.y = 0.2;
+  bullet.x = rocket.x + 22;
+  bullet.y = 450;
+  bullet.scale.x = 0.05;
+  bullet.scale.y = 0.05;
   app.stage.addChild(bullet);
   flyUp(bullet, 5);
 
